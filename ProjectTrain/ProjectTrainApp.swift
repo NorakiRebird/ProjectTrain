31717e284e1506f86d0ba9073439d122df1a0a57
NorakiRebird <Rayann_Chaghla@outlook.fr>
2025-03-10 09:07:56 +0100

    Initial Commit

---
+++ ProjectTrain/ProjectTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTrainApp.swift
+//  ProjectTrain
+//
+//  Created by Rayann chaghla on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
